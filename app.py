d38fe836db711437dc22c271bd9bd4cbdb159c32
Patrick Tinsley <patrick.g.tinsley@gmail.com>
2022-03-16 18:06:12 -0400

    time

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+
+from flask import Flask, request, render_template
+
+import time
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello!'
+@app.route("/")
+def index():
+    return render_template("index.html")
 
+@app.route("/getTime", methods=['GET'])
+def getTime():
+    print("browser time: ", request.args.get("time"))
+    print("server time : ", time.strftime('%A %B, %d %Y %H:%M:%S'));
+    return "Done"
